afddb7baecc62ac8b5f214eb5abd7440924bcbf1
Dion Chaika <49035785+dionchaika@users.noreply.github.com>
2019-05-06 18:31:54 +0300

    Update Container.php

--- src/Container.php
+++ src/Container.php
@@ -13,7 +13,22 @@ namespace Dionchaika\Container;
 
 use Psr\Container\ContainerInterface;
 
+/**
+ * <code>
+ *      $container = new Container;
+ *
+ *      $container->bind('db', \PDO::class)
+ *          ->asSingleton()
+ *          ->bindParameter('dsn', 'mysql:host=localhost')
+ *          ->bindParameter('username', $username)
+ *          ->bindParameter('passwd', $passwd);
+ *
+ *      if ($container->has('db')) {
+ *          $db = $container->get('db');
+ *      }
+ * </code>
+ */
 class Container implements ContainerInterface
 {
-    //
+    
 }
